299d16fd668471a4c1dc0151cc8373f146d0172d
Zohaibiqbal18 <iamzohaibiqbal18@gmail.com>
2025-05-12 23:02:04 +0500

    Add button toggler for small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/welcome">{props.title}</Link>
-        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
           aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button> */}
+        </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
